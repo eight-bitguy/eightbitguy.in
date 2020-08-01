7e654e9fb65921d0a05cfff0f8f07584870ff4f9
Sandal <sandal@technogise.com>
2020-08-02 02:10:06 +0530

    updated circle-ci config file

--- src/JS/App.js
+++ src/JS/App.js
@@ -5,7 +5,7 @@ const App = () => {
     return (
         <div className="app">
             Shanu Jain is awesome
-            <ParticlesBg type="lines" bg={true}/>
+            {/*<ParticlesBg type="lines" bg={true}/>*/}
         </div>
     );
 };
