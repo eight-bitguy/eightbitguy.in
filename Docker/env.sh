eae7dd0623009a9e14c7c36b11e3d06fe3e19190
Sandal <sandal@technogise.com>
2020-09-03 12:11:17 +0530

    job fixed

--- Docker/env.sh
+++ Docker/env.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
 
-> /usr/share/nginx/html/env-config.js
-env >> .env
-echo "window._env_ = {" >> /usr/share/nginx/html/env_config.js
+> /var/www/html/env_config.js
+echo "window._env_ = {" >> /var/www/html/env_config.js
 
-# Read each line in .env file
-# Each line represents key=value pairs
-while read -r line || [[ -n "$line" ]];
-do
-  # Split env variables by character `=`
-  if printf '%s\n' "$line" | grep -q -e '='; then
-    varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
-    varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
-  fi
+hostnameValue=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/hostname" -H 'Metadata-Flavor':'Google')
+echo "   hostname: \"$hostnameValue\"" >> /var/www/html/env_config.js
 
-  # Read value of current variable if exists as Environment variable
-  value=$(printf '%s\n' "${!varname}")
-  # Otherwise use value from .env file
-  [[ -z $value ]] && value=${varvalue}
-
-  # Append configuration property to JS file
-  echo "  $varname: \"$value\"," >> /usr/share/nginx/html/env_config.js
-done < .env
-
-rm .env
-
-echo "}" >> /usr/share/nginx/html/env_config.js
+echo "}" >> /var/www/html/env_config.js
