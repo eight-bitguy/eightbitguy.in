a753a21848e199268d67c32025fb3ebdcb4df04b
sandal jain <sandal@technogise.com>
2021-05-14 16:17:05 +0530

    Added title icon

--- src/Components/DownloadResume.js
+++ src/Components/DownloadResume.js
@@ -3,13 +3,15 @@ import DownloadIcon from "../Images/DownloadIcon";
 
 const DownLoadResume = () => {
     return (
-        <div>
-            <button className='my-button'>
-                <div className='download-resume-button'>
-                    <DownloadIcon /> <span>Resume</span>
-                </div>
-            </button>
-        </div>
+            <div>
+                <button className='my-button'>
+                    <a type="button" className="download-resume-button-container" href='./Sandal_Jain.pdf' download>
+                        <div className='download-resume-button'>
+                            <DownloadIcon /> <span>Resume</span>
+                        </div>
+                    </a>
+                </button>
+            </div>
     );
 };
 
