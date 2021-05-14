cf47c66f1a0b98b069fc8fe2a7bcc7fccf963a06
sandal jain <sandal@technogise.com>
2021-05-14 12:21:44 +0530

    Added page for resume

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -1,11 +1,11 @@
 import React from 'preact/compat';
-import {useState} from "preact/hooks";
+// import {useState} from "preact/hooks";
 import {route} from "preact-router";
 import Url from "../JS/Url";
 import DownloadIcon from "../Images/DownloadIcon";
-import Line from "../Components/line";
-import InputWithoutLabel from "../Components/InputWithoutLabel";
-import MyButton from "../Components/MyButton";
+// import Line from "../Components/line";
+// import InputWithoutLabel from "../Components/InputWithoutLabel";
+// import MyButton from "../Components/MyButton";
 import BackButton from "../Images/backbutton";
 
 const onClick = () => {
@@ -13,19 +13,19 @@ const onClick = () => {
 };
 
 const Resume = () => {
-    const [email, setEmail] = useState('');
-    const [loading, setLoading] = useState(false);
-
-    const sendResume = () => {
-        if (loading || !email) {
-            return;
-        }
-
-        setLoading(true);
-        setTimeout(() => setLoading(false), 1000);
-        // setLoading(false);
-    };
-
+    // const [email, setEmail] = useState('');
+    // const [loading, setLoading] = useState(false);
+    //
+    // const sendResume = () => {
+    //     if (loading || !email) {
+    //         return;
+    //     }
+    //
+    //     setLoading(true);
+    //     setTimeout(() => setLoading(false), 1000);
+    //     // setLoading(false);
+    // };
+    //
     return (
         <div className='resume-container' style={{height: '100vh'}}>
             <div className='row-0'>
